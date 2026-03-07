87d62e16821ad29e555801d716fa210e76ff1243
jamilagibson <jamila.l.gibson@gmail.com>
2026-03-06 22:20:16 -0500

    feat: add POST /votes endpoint

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.use('/users', usersRouter);
 const requestsRouter = require('./routes/requests');
 app.use('/requests', requestsRouter);
 
+const votesRouter = require('./routes/votes');
+app.use('/votes', votesRouter);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
